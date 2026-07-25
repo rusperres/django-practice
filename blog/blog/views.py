dc18b139af8a77cd71bc19cc00706f669132ded4
rusperres <jairusjaspervc@gmail.com>
2026-07-26 04:08:55 +0800

    blog: added blog detail page

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import *
 # Create your views here.
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+    context_object_name = 'post'
 
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
