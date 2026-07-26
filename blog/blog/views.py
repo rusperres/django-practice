fc8cf2f8a938d7803f96db259f4ae52fefa067fa
rusperres <jairusjaspervc@gmail.com>
2026-07-26 12:31:59 +0800

    blog: added delete form

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import *
+from django.urls import reverse_lazy
 # Create your views here.
 
 class BlogListView(ListView):
@@ -23,3 +24,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
