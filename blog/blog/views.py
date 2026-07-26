1fea7b6c5c67c773fff22ef981d6896175c9b8dc
rusperres <jairusjaspervc@gmail.com>
2026-07-26 12:24:39 +0800

    blog: added update form

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import *
 # Create your views here.
 
@@ -18,3 +18,8 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
